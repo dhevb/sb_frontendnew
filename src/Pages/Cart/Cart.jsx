c9dde875ba0a890d74b595a20100475400b9cd25
yash vardhan <yashvrdhn18@gmail.com>
2024-05-16 10:57:47 +0530

    done

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/footer'
 import SingleBanner from '../../components/Banners/SingleBanner'
 import coverphoto from"../../assets/coverphoto.jpg"
 
 const Cart = () => {
+
+  const getcartitemsfromlocalstorage =()=>{
+    let cart = JSON.parse(localStorage.getItem("cart"))
+
+    if(cart){
+      console.log(cart)
+    }
+  }
+
+  useEffect(()=>{
+    getcartitemsfromlocalstorage()
+  },[])
   return (
     <div>
       <Navbar/>
